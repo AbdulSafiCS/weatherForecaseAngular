e4ea03a3f2cce2d41d0c18c94d3ac412201ba79a
abdulsafics <hasibsaf90@gmail.com>
2024-09-20 15:36:04 -0700

    added the weather, city, and country components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
-}
+
 
 @Component({
   selector: 'app-root',
@@ -17,25 +12,6 @@ interface WeatherForecast {
   styleUrl: './app.component.scss'
 })
 
-export class AppComponent implements OnInit {
-  public forecasts: WeatherForecast[] = [];
-
-  constructor(private http: HttpClient) {}
-
-  ngOnInit() {
-    this.getForecasts();
-  }
-
-  getForecasts() {
-    this.http.get<WeatherForecast[]>('http://localhost:5186/weatherforecast').subscribe(
-      (result) => {
-        this.forecasts = result;
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
-  }
-
-  title = 'myfirstangularapp.client';
+export class AppComponent  {
+  
 }
